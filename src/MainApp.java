b6ba96afde74ea9a02f4320e1919d16f72ee660a
Çağla <mariliith@hotmail.com>
2022-10-11 20:27:31 +0300

    Dünya değişti.

---
+++ src/MainApp.java
@@ -0,0 +1,17 @@
+//Bu bir java sınıfıdır
+public class MainApp {
+    //Bu metot programı başlatır
+    public static void main(String[] args) {
+        /*
+        Araya not almak için; ctrl shift /
+         */
+        // /* içine aldığımız şeyler de koddur ancak java değil dökümantasyon kodu oluyor.
+// ctrl d yaparsak imcelin üzerinde bulunan metni aşşa copy paste yapar
+//        Sadece satır notu için ctrl /
+        System.out.println("Merhaba...");
+        System.out.println("Dünya");
+
+        //TODO: Yapacağımız işlemi hatırlamak için TODO yazıp : koyuyoruz. Aşağıda bize bir liste açar
+
+    }
+}
